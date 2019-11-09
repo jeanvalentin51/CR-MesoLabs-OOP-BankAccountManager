6104e657609c921d09ea24f950ccceb50abc5f1b
MikeGray31 <michaelkgray31@gmail.com>
2019-11-09 16:54:57 -0500

    Added deposit, withdrawal, and transfer methods to Account class

--- src/main/java/account/Account.java
+++ src/main/java/account/Account.java
@@ -13,17 +13,69 @@ public abstract class Account {
 
     private String accountType;
 
-    protected void createAccount(){
-        accountNumber = Long.valueOf(100);
+    public Account(String userName, ){
+        this.accountNumber = Long.valueOf(100);
     }
+
+//    protected void createAccount(){
+//        accountNumber = Long.valueOf(100);
+//    }
+
+    protected void deposit(double amount) throws IllegalArgumentException {
+        if(amount <= 0){
+            throw new IllegalArgumentException("Deposit amount must be a number greater than zero.");
+        }
+        else if(amount > 0){
+            this.setBalance(this.balance + amount);
+            this.accountHistory.recordHistoryDeposit(amount);
+        }
+    }
+
+    protected void withdraw(double amount) throws IllegalArgumentException{
+        if((amount > this.getBalance() && this.overdraft == 1) || amount <= 0){
+            throw new IllegalArgumentException("Withdrawal amount must be greater than zero and less than your current balance.");
+        }
+        else if(amount <this.getBalance() && amount > 0){
+            this.setBalance(this.balance - amount);
+            this.accountHistory.recordHistoryWithdraw(amount);
+        }
+    }
+
+    protected void transferFromThisAccount(Account toAccount, double amount) throws IllegalArgumentException{
+        if(amount > this.getBalance() || amount <= 0){
+            throw new IllegalArgumentException("Withdrawal amount must be greater than zero and less than this account's current balance.");
+        }
+        else if(amount <this.getBalance() && amount > 0){
+            this.setBalance(this.getBalance() - amount);
+            this.accountHistory.recordHistoryOutgoingTransfer(amount, toAccount.getAccountNumber());
+            toAccount.setBalance(toAccount.getBalance() + amount);
+            toAccount.accountHistory.recordHistoryIncomingTransfer(amount, this.getAccountNumber());
+        }
+    }
+
+    protected void transferToThisAccount(Account fromAccount, double amount) throws IllegalArgumentException{
+        if(amount > fromAccount.getBalance() || amount <= 0){
+            throw new IllegalArgumentException("Withdrawal amount must be greater than zero and less than the source account's current balance.");
+        }
+        else if(amount <fromAccount.getBalance() && amount > 0){
+            this.setBalance(this.getBalance() + amount);
+            this.accountHistory.recordHistoryIncomingTransfer(amount, fromAccount.getAccountNumber());
+            fromAccount.setBalance(this.getBalance() - amount);
+            fromAccount.accountHistory.recordHistoryOutgoingTransfer(amount, this.getAccountNumber());
+        }
+    }
+
+
+    protected void notThis(){
+
+    }
+
+
+    //getters and setters
 
     public Long getAccountNumber() {
         return accountNumber;
     }
-
-//    public void setAccountNumber(Long accountNumber) {
-//        this.accountNumber = accountNumber;
-//    }
 
     public Double getBalance() {
         return balance;
