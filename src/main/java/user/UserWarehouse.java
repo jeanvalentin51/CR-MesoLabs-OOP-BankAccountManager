67f1c38867ddbe716631997db7810efc4250c305
Kwamè Warrick <kwame@zipcoders-MacBook-Pro-108.local>
2019-11-09 18:36:49 -0500

    finished user profile , may need to change HashMap and password to Pin

--- src/main/java/user/UserWarehouse.java
+++ src/main/java/user/UserWarehouse.java
@@ -1,7 +1,29 @@
 package user;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class UserWarehouse {
 
-    private User newUser;
+    private volatile Map<String, UserProfile> userProfiles = new HashMap<>();
+
+    public void add(UserProfile userProfile) {
+        userProfiles.put(userProfile.getFirstName(),userProfile);
+    }
+
+
+    public Boolean validatePin(int userProfilePinNumber, String password) {
+        UserProfile userProfile = null;
+         userProfilePinNumber = userProfile.getPinNumber();
+
+        return password.equals(userProfilePinNumber);
+    }
+
+
+    public void removeUserProfileByUserName(String userName) {
+        userProfiles.remove(userName);
+    }
 
 }
