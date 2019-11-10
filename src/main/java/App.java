c04ada94ddb486ee74d65e58ff4739ee8a124370
MannyMonocle <eetumadu@gmail.com>
2019-11-10 14:50:09 -0500

    started

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,17 @@
+import java.util.Scanner;
+
 public class App {
+
+    private Scanner input = new Scanner(System.in);
+    private void output(String msg) { System.out.println(msg); };
+    private String onScreenMsg;
+
+    public void App(){
+        output(welcome);
+
+    }
+
+    //ASCII
+    String welcome = "Thank You For Banking With 5ive";
+
 }
---
+++ src/test/java/utilities/AppTest.java
@@ -0,0 +1,7 @@
+package utilities;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+
+}
