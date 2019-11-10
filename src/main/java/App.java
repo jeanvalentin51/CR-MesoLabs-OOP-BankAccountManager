e6a836f9ce5c741e61304214bac22e4769cf2ab4
MannyMonocle <eetumadu@gmail.com>
2019-11-10 16:00:03 -0500

    main menu added

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,13 +5,44 @@ public class App {
     private Scanner input = new Scanner(System.in);
     private void output(String msg) { System.out.println(msg); };
     private String onScreenMsg;
+    private Object choice;
 
     public void App(){
         output(welcome);
+        mainMenu();
 
     }
 
-    //ASCII
+
+    // MENUS
+    public void mainMenu(){
+        output(mainMenu);
+        switch ( input.nextInt() ){
+            case 1 :
+                output("Logging in...");
+                //logginMenu();
+                break;
+            case 2 :
+                output("Let's get you set up");
+                //newUserMenu();
+                break;
+            case 3 :
+                output("Thank you for banking with us");
+                break;
+            default:
+                output("Please try again");
+                break;
+
+        }
+    }
+
+
+
+    //STRINGS
     String welcome = "Thank You For Banking With 5ive";
+    String mainMenu = "What would you like to do?\n" +
+            "1 - Log in\n" +
+            "2 - Create Account\n" +
+            "3 - Exit";
 
 }
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,7 +1,9 @@
-package utilities;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
+    App app = new App();
+
 
 }
